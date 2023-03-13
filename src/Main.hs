afdc0df9c3141055f8a5a87b96766b1a5332a9ac
Daniele Anselmo <daniele.anselmo91@gmail.com>
2023-03-13 17:19:29 +0100

    monomer-prova

--- src/Main.hs
+++ src/Main.hs
@@ -10,6 +10,7 @@ import Monomer
 import TextShow
 
 import qualified Monomer.Lens as L
+import Monomer.Core.Lens (HasFontSize(fontSize))
 
 newtype AppModel = AppModel {
   _clickCount :: Int
@@ -22,27 +23,25 @@ data AppEvent
 
 makeLenses 'AppModel
 
-buildUI
-  :: WidgetEnv AppModel AppEvent
-  -> AppModel
-  -> WidgetNode AppModel AppEvent
+buildUI :: WidgetEnv AppModel AppEvent -> AppModel -> WidgetNode AppModel AppEvent
 buildUI wenv model = widgetTree where
-  widgetTree = vstack [
+  widgetTree = hstack [vstack [
       label "Hello world",
       spacer,
       hstack [
         label $ "Click count: " <> showt (model ^. clickCount),
         spacer,
         button "Increase count" AppIncrease
+      ],
+      spacer,
+      vstack_ [childSpacing_ 20] [
+        label "this is a line",
+        label "this is a another line"
       ]
-    ] `styleBasic` [padding 10]
+    ] `styleBasic` [padding 10],
+    widgetIf (model ^. clickCount >= 5) (label "STOP FUCKING CLICKING!") `styleBasic` styles model]
 
-handleEvent
-  :: WidgetEnv AppModel AppEvent
-  -> WidgetNode AppModel AppEvent
-  -> AppModel
-  -> AppEvent
-  -> [AppEventResponse AppModel AppEvent]
+handleEvent :: WidgetEnv AppModel AppEvent -> WidgetNode AppModel AppEvent -> AppModel -> AppEvent -> [AppEventResponse AppModel AppEvent]
 handleEvent wenv node model evt = case evt of
   AppInit -> []
   AppIncrease -> [Model (model & clickCount +~ 1)]
@@ -52,10 +51,20 @@ main = do
   startApp model handleEvent buildUI config
   where
     config = [
-      appWindowTitle "Hello world",
+      appWindowTitle "Home Management",
       appWindowIcon "./assets/images/icon.png",
       appTheme darkTheme,
       appFontDef "Regular" "./assets/fonts/Roboto-Regular.ttf",
       appInitEvent AppInit
       ]
     model = AppModel 0
+
+
+moreThanTen :: AppModel -> StyleState
+moreThanTen model = styleIf (model ^. clickCount >= 10) (textColor red)
+
+moreThanFifteen :: AppModel -> StyleState
+moreThanFifteen model = styleIf (model ^. clickCount >= 15) (textSize 30)
+
+styles :: AppModel -> [StyleState]
+styles x = [moreThanTen x, moreThanFifteen x]
